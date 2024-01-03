bbf44c8359979d3fe6867a5f932e1a1c58a05896
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2024-01-03 12:47:16 +0530

    Exploring CustomPermission class to make secure api

--- authapi/v3/Readme.md
+++ authapi/v3/Readme.md
@@ -1,5 +1,10 @@
-## CustomPermission class
+## CustomPermission class inherit from 
 
+```
+from rest_framework.permissions import BasePermission
+```
+
+###### Define either one or both method (return True to grant access)
 ```
 has_permission(self, request, view)
 ```
--- authapi/v3/views.py
+++ authapi/v3/views.py
@@ -1,11 +1,11 @@
 from rest_framework.viewsets import ModelViewSet
 from api.serializers import  Student, StudentSerializer
 from rest_framework.authentication import SessionAuthentication
-from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from .custompermissions import MyPermission
 
 
 class StudentViewSet(ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
     authentication_classes = [SessionAuthentication]
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [MyPermission]
